cce287a8a40035fa2e75bd956c49bae8b3439a6e
Jaime E Falla <65088216+Jaimedfalla@users.noreply.github.com>
2022-03-23 23:07:37 -0500

    Project Integrator. Users booking api

---
+++ src/main/java/com/example/usersbooking/UsersBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.usersbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/usersbooking/UsersBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usersbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
